3d97e0ebfe96ef43ae3c7a619fd891fb67cb67c2
examforge99 <examforge99@gmail.com>
2026-07-07 23:30:38 +0100

    Remove StatCards component from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar";
-import StatCards from "@/components/StatCards";
 import PracticeModes from "@/components/PracticeModes";
 import ScoreDistribution from "@/components/ScoreDistribution";
 import RecentTests from "@/components/RecentTests";
@@ -11,7 +10,7 @@ export default function DashboardPage() {
       <Navbar />
 
       <main>
-        <StatCards />
+    
         <PracticeModes />
 
         <div
