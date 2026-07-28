c5128f1ca349ccdaefd3abd93fa8cf66486f5866
examforge99 <examforge99@gmail.com>
2026-07-28 21:16:02 +0100

    Create server.ts

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,10 @@
+// utils/supabase/server.ts
+
+import { createClient } from "@supabase/supabase-js";
+
+export function createServerSupabase() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
